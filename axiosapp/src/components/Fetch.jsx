367bed8bdda18f3790716fc2b422667d383bec65
Mohammadzaki0027 <zaki7875@gmail.com>
2022-07-14 20:52:02 +0530

    updateaxios

--- axiosapp/src/components/Fetch.jsx
+++ axiosapp/src/components/Fetch.jsx
@@ -4,10 +4,11 @@ import Show from "./Show";
 const Fetch = () => {
   const [query, setQuery] = React.useState("albert");
   const [data, setData] = React.useState([]);
-
+const[page,SetPage]=React.useState(10)
+const[num,Setnum]=React.useState(3)
   const getdata = async () => {
     let r = await axios.get(
-      "https://api.github.com/search/users?" + `q=${query}`
+      "https://api.github.com/search/users?" +`q=${query}&page=${num}&per_page=${page}`
     );
     console.log("axios", r.data.items);
     setData(r.data.items);
@@ -16,13 +17,16 @@ const Fetch = () => {
   React.useEffect(() => {
     setTimeout(() => {
       getdata();
+      console.log(num)
     }, 2000);
-  }, [query]);
+  }, [query,num,page]);
   const handleclick = (e) => {
     console.log(e.target.value);
     setQuery(e.target.value);
   };
-
+const handlevaluefor=(value)=>{
+  Setnum(num+value)
+}
   return (
     <div>
       <input
@@ -32,6 +36,9 @@ const Fetch = () => {
       />
 
       <Show data={data}></Show>
+      <button onClick={()=>{handlevaluefor(1)}}>Forward</button>
+       <button onClick={()=>{handlevalueprev(-1)}} disabled={page===10}>Prev</button>
+
     </div>
   );
 };
